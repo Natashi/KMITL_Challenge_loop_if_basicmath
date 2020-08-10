5cad72efda91316f0a99be1144100a64c2c9a933
Natashi <32347635+Natashi@users.noreply.github.com>
2020-08-10 13:58:27 +0700

    Shuts up warnings and errors

--- Challenge_loop_if_basicmath/pch.h
+++ Challenge_loop_if_basicmath/pch.h
@@ -1,4 +1,8 @@
 #pragma once
 
+#pragma warning (disable : 26451)	//Arithmetic overflow
+
+#define _CRT_SECURE_NO_WARNINGS		//Shuts up "This XXX may be unsafe" warnings
+
 #include <iostream>
 #include <cmath>
