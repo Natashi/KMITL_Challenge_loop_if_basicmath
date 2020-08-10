52d1459ea722797ea77aad78f86cc8f91c2356ca
Natashi <32347635+Natashi@users.noreply.github.com>
2020-08-10 13:49:50 +0700

    Add project files.

---
+++ Challenge_loop_if_basicmath/Challenge_loop_if_basicmath.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+
+using namespace std;
+
+int main() {
+    int bound1;
+    int bound2;
+    scanf("%d %d", &bound1, &bound2);
+
+    size_t count = 0;
+    double sum_sq = 0.0;
+    double mean = (bound1 + bound2) / 2.0;
+
+    auto AddResult = [&](int x) {
+        printf("%d ", x);
+        ++count;
+
+        double diff = x - mean;
+        sum_sq += diff * diff;
+    };
+
+    if (bound1 < bound2) {
+        for (int i = bound1; i <= bound2; ++i)
+            AddResult(i);
+    }
+    else {
+        for (int i = bound1; i >= bound2; --i)
+            AddResult(i);
+    }
+    
+    printf("\nAverage = %.1f", mean);
+
+    double deviation = count > 0 ? (sqrt(sum_sq) / count) : 0.0;
+    printf("\nSD = %.2f", deviation);
+}
